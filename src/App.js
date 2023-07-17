f1cfa287af9b28dc5bf32510261a787a19b42f42
sachinmaile <maile.sachin16@gmail.com>
2023-07-17 17:24:29 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import TooltipComponent from "./ToolTip";
+
 function App() {
+  const [position, setPosition] = useState("left");
+
+  const handleButtonClick = (value) => {
+    setPosition(value);
+  };
+
   return (
     <div className="App">
-      
+      <TooltipComponent position={position} />
+      <ButtonGroup aria-label="Button Group" id="buttons">
+        <Button variant="primary" onClick={() => handleButtonClick("left")}>
+          Left
+        </Button>
+        <Button variant="secondary" onClick={() => handleButtonClick("top")}>
+          Top
+        </Button>
+        <Button variant="success" onClick={() => handleButtonClick("right")}>
+          Right
+        </Button>
+        <Button variant="warning" onClick={() => handleButtonClick("bottom")}>
+          Bottom
+        </Button>
+      </ButtonGroup>
     </div>
   );
 }
